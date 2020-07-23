ae032de30edb02fc43369759efe5673e762f98f5
Augusto Emiliano Protillo <aportillo@desarrollosnea.com>
2020-07-22 22:22:26 -0300

    add mercadopago

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+const dotenv = require("dotenv");
 var app = express();
+const bodyParser = require("body-parser");
+
+const PaymentController = require("./controllers/PaymentController");
+//importamos el controller
+
+const PaymentService = require("./services/PaymentService");
+//importamos el service
+
+const PaymentInstance = new PaymentController(new PaymentService());
+// Permitimos que el controller pueda usar el service
+
+dotenv.config();
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -18,4 +33,12 @@ app.use(express.static("assets"));
 
 app.use("/assets", express.static(__dirname + "/assets"));
 
+//MERCADO PAGO
+app.post("/payment/new", (req, res) =>
+  PaymentInstance.getMercadoPagoLink(req, res)
+);
+app.post("/webhook", (req, res) => PaymentInstance.webhook(req, res));
+
 app.listen(process.env.PORT || 3000);
+
+console.log("Puerto > ", process.env.PORT || 3000);
